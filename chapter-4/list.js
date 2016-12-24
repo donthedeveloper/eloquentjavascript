3f8d70d636245b6d825cea979e5669f034eec01a
Don Hansen <donhansen347@gmail.com>
2016-12-24 03:46:06 -0500

    Completely finished 'A list' - Yay I'm getting a better grasp of recursion.

--- chapter-4/list.js
+++ chapter-4/list.js
@@ -27,6 +27,17 @@ function listToArray(list) {
   return returnArr;
 }
 
+// this is the clever version of the function above given by the hints, i love it!
+function listToArray(list) {
+  var arr = [];
+  
+  for (var node = list; node; node = node.rest) {
+    arr.push(node.value);
+  }
+  
+  return arr;
+}
+
 
 function prepend(elem, list) {
   if (list === null) {
@@ -39,6 +50,16 @@ function prepend(elem, list) {
 
 function nth(list, index) {
   return listToArray(list)[index];
+}
+
+// this is the recursive version of nth hinted by the.. hints!
+function nth(list, index) {
+  if (index === 0) {
+    console.log(list);
+    return list.value;
+  } else {
+    return nth(list.rest, index-1);
+  }
 }
 
 
