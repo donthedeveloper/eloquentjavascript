0e1604de93fc27ee085b785609192ea97266859d
Don Hansen <donhansen347@gmail.com>
2016-12-24 00:08:10 -0500

    thoughts after looking at hints provided

--- chapter-4/sumofrange.js
+++ chapter-4/sumofrange.js
@@ -38,3 +38,7 @@ function sum(arr) {
 console.log( sum( range(1, 10) ) );
 console.log( sum( range(1, 10, 2) ) );
 console.log( sum( range(5, 2, -1) ) );
+
+
+// side notes after looking at hints
+/* Interesting - I didn't account for an infinite loop being creating with a range of (5, 2). Also, it suggested using two loops, but instead, I created a function to check if we were looking for the correct condition EACH loop, which may hog more resources than needed. */
