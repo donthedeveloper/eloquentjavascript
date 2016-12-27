d15510775b5a4b21fa961337de38f4a8ff228fe5
Don Hansen <donhansen347@gmail.com>
2016-12-26 19:40:32 -0500

    Completed 2nd part of 'Chess board', changings arguments to 'width' & 'height'

--- chapter-2/chessboard.js
+++ chapter-2/chessboard.js
@@ -4,11 +4,11 @@ function loopThrough(num) {
   }
 }
 
-function createChessBoard(num) {
-  for (var i = 1, output = ''; i <= num; i++) {
+function createChessBoard(width, height) {
+  for (var i = 1, output = ''; i <= height; i++) {
     
     if (i % 2 === 0) {
-      while (output.length < num) {
+      while (output.length < width) {
         if ( output.charAt(output.length - 1) === '#') {
           output += ' ';
         } else {
@@ -16,7 +16,7 @@ function createChessBoard(num) {
         }
       }
     } else {
-      while(output.length < num) {
+      while(output.length < width) {
         if ( output.charAt(output.length - 1) === ' ') {
           output += '#';
         } else {
@@ -30,4 +30,4 @@ function createChessBoard(num) {
   }
 }
 
-createChessBoard(5);
+createChessBoard(2, 8);
