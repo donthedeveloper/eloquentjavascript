85756c17618b78198c1654eaa2d5de57bb8f37d0
Don Hansen <donhansen347@gmail.com>
2016-12-27 21:43:09 -0500

    updated exercise 1 in chapter 3

--- chapter-3/minimum.js
+++ chapter-3/minimum.js
@@ -1,9 +1,5 @@
-function min(num1, num2) {
-  if (num1 > num2) {
-    return num2;
-  } else {
-    return num1;
-  }
+function min(num1, num2) { 
+  return num1 > num2 ? num2 : num1;
 }
 
 console.log(min(0, 10));
