0753979b2836e839715e4542d4cd96a0f4f6f7b1
Don Hansen <donhansen347@gmail.com>
2016-12-27 21:57:28 -0500

    Completed 'Recursion' exercise in chapter 3

--- chapter-3/minimum.js
+++ chapter-3/minimum.js
@@ -2,5 +2,11 @@ function min(num1, num2) {
   return num1 > num2 ? num2 : num1;
 }
 
-console.log(min(0, 10));
-console.log(min(0, -10));
+
+function isEven(num) {
+  return num % 2 ? false : true;
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
